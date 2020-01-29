07af449a41d608edd2db01eeaf5ae5aa80990748
2min0r <wwft09@naver.com>
2020-01-29 23:58:22 +0900

    Update algorithm practice code to Programmers

--- Python/Algorithm/programmers/targetnumber.py
+++ Python/Algorithm/programmers/targetnumber.py
@@ -1,9 +1,11 @@
 # [targetnumber.py]
 # 타겟넘버
 
+
+# first
 from itertools import combinations
 
-def solution(numbers, target):
+def solution1(numbers, target):
     answer = 0
     for i in range(0, len(numbers)-1):
         for j in list(combinations(numbers, i)):
